6ea7e8b2d677921e2321b5ec50d7eb92dee937c0
devguru <devguru.kim@gmail.com>
2022-01-25 01:17:54 +1300

    Add examples

---
+++ ThreadPooling/ThreadPooling/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadPooling
+{
+    internal class Program
+    {
+        static void MainThread(object state)
+        {
+            for(int i=0; i<5; i++)
+            {
+                Console.WriteLine("Hello Thread");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ThreadPool.QueueUserWorkItem(MainThread);
+
+            while (true)
+            {
+
+            }
+        }
+    }
+}
---
+++ ThreadingBasic/ThreadingBasic/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadingBasic
+{
+    internal class Program
+    {
+        static void MainThread()
+        {
+            while (true)
+                Console.WriteLine("Hello Thread");
+        }
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(MainThread);
+            t.IsBackground = true;
+            t.Start();
+
+            Console.WriteLine("Hello World");
+        }
+    }
+}
